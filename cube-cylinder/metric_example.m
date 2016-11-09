6937ecfda519d92c30d52a5828776177d7405a81
Mike Park <Mike.Park@NASA.Gov>
2016-11-09 08:13:51 -0500

    finished example

--- cube-cylinder/metric_example.m
+++ cube-cylinder/metric_example.m
@@ -9,9 +9,9 @@ y=0.0
 r=sqrt(x^2+y^2)
 t=atan2(y,x)
 h_r = h0 + 2*(0.1-h0)*abs(r-0.5)
-hs = [  h_t^-2   0      0    ;
-         0     h_r^-2   0    ;
-         0      0     h_z^-2 ]
+hs = [  h_r^-2   0      0    ;
+         0     h_t^-2   0    ;
+         0       0     h_z^-2 ]
 R = [ cos(t) -sin(t)   0   ;
       sin(t)  cos(t)   0   ;
         0       0      1   ]
@@ -22,9 +22,22 @@ y=0.5
 r=sqrt(x^2+y^2)
 t=atan2(y,x)
 h_r = h0 + 2*(0.1-h0)*abs(r-0.5)
-hs = [  h_t^-2   0      0    ;
-         0     h_r^-2   0    ;
-         0      0     h_z^-2 ]
+hs = [  h_r^-2   0      0    ;
+         0     h_t^-2   0    ;
+         0       0     h_z^-2 ]
+R = [ cos(t) -sin(t)   0   ;
+      sin(t)  cos(t)   0   ;
+        0       0      1   ]
+M = R*hs*(R')
+
+x=1.0
+y=1.0
+r=sqrt(x^2+y^2)
+t=atan2(y,x)
+h_r = h0 + 2*(0.1-h0)*abs(r-0.5)
+hs = [  h_r^-2   0      0    ;
+         0     h_t^-2   0    ;
+         0       0     h_z^-2 ]
 R = [ cos(t) -sin(t)   0   ;
       sin(t)  cos(t)   0   ;
         0       0      1   ]
